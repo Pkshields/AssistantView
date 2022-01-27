76f00b746fa7a3d8d23d9392719c381e1daf881f
Paul Shields <pkshields@gmail.com>
2022-01-27 19:04:17 +0000

    Adding a flow to let AssistantView wait until all classes are indexed in the project before trying to find any corresponding files in AssistantView.

--- src/main/kotlin/dev/paulshields/assistantview/FileOpenedListener.kt
+++ src/main/kotlin/dev/paulshields/assistantview/FileOpenedListener.kt
@@ -1,7 +1,11 @@
 package dev.paulshields.assistantview
 
+import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.fileEditor.FileEditorManagerEvent
 import com.intellij.openapi.fileEditor.FileEditorManagerListener
+import com.intellij.openapi.project.DumbService
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
 import dev.paulshields.assistantview.common.Dispatcher
 import dev.paulshields.assistantview.extensions.runOnUiThread
 import dev.paulshields.assistantview.extensions.runWithReadPermission
@@ -10,6 +14,7 @@ import dev.paulshields.assistantview.services.FileAssistantService
 import dev.paulshields.assistantview.services.FileManagerService
 import dev.paulshields.assistantview.sourcefiles.AssistantViewFile
 import dev.paulshields.lok.logInfo
+import org.jetbrains.kotlin.idea.caches.resolve.util.isInDumbMode
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
@@ -22,18 +27,32 @@ class FileOpenedListener : FileEditorManagerListener, KoinComponent {
     override fun selectionChanged(event: FileEditorManagerEvent) {
         if (event.newFile == null) return
 
-        dispatcher.runOnBackgroundThread {
-            val openedFile = getFileFromFileEvent(event)
-            val counterpartFile = openedFile?.let { getCounterpartFile(it) }
-
-            counterpartFile?.let { openFileInAssistantView(it) }
-
-            logInfo { "File opened: ${openedFile?.fileName}; counterpart file: ${counterpartFile?.fileName}" }
+        if (event.manager.project.isInDumbMode()) {
+            openCounterpartFileWhenProjectExitsDumbMode(event.manager)
+            return
         }
+
+        findAndOpenCounterpartFile(event.newFile, event.manager.project)
     }
 
-    private fun getFileFromFileEvent(event: FileEditorManagerEvent) =
-        runWithReadPermission { fileManagerService.getFileFromVirtualFile(event.newFile, event.manager.project) }
+    private fun openCounterpartFileWhenProjectExitsDumbMode(fileEditorManager: FileEditorManager) =
+        DumbService.getInstance(fileEditorManager.project).runWhenSmart {
+            fileEditorManager.selectedEditor?.file?.let {
+                findAndOpenCounterpartFile(it, fileEditorManager.project)
+            } ?: logInfo { "Dumb mode has finished but no file is open for project ${fileEditorManager.project.name}. Ignoring." }
+        }
+
+    private fun findAndOpenCounterpartFile(rawFile: VirtualFile, project: Project) = dispatcher.runOnBackgroundThread {
+        val openedFile = getAssistantViewFile(rawFile, project)
+        val counterpartFile = openedFile?.let { getCounterpartFile(it) }
+
+        counterpartFile?.let { openFileInAssistantView(it) }
+
+        logInfo { "File opened: ${openedFile?.fileName}; counterpart file: ${counterpartFile?.fileName}" }
+    }
+
+    private fun getAssistantViewFile(rawFile: VirtualFile, project: Project) =
+        runWithReadPermission { fileManagerService.getFileFromVirtualFile(rawFile, project) }
 
     private fun getCounterpartFile(file: AssistantViewFile) =
         runWithReadPermission { fileAssistantService.getCounterpartFile(file) }
--- src/test/kotlin/dev/paulshields/assistantview/FileOpenedListenerTest.kt
+++ src/test/kotlin/dev/paulshields/assistantview/FileOpenedListenerTest.kt
@@ -1,6 +1,8 @@
 package dev.paulshields.assistantview
 
+import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.fileEditor.FileEditorManagerEvent
+import com.intellij.openapi.project.DumbService
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import dev.paulshields.assistantview.common.Dispatcher
@@ -15,6 +17,7 @@ import dev.paulshields.assistantview.testcommon.mockKoinApplication
 import io.mockk.every
 import io.mockk.mockkStatic
 import io.mockk.verify
+import org.jetbrains.kotlin.idea.caches.resolve.util.isInDumbMode
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.RegisterExtension
@@ -24,10 +27,21 @@ import org.koin.test.KoinTest
 class FileOpenedListenerTest : KoinTest {
     private val assistantViewFile = mock<AssistantViewFile>()
     private val virtualFile = mock<VirtualFile>()
-    private val project = mock<Project>()
+    private val dumbService = mock<DumbService>().apply {
+        every { runWhenSmart(any()) } answers { firstArg<Runnable>().run() }
+    }
+    private val project = mock<Project>().apply {
+        mockkStatic(Project::isInDumbMode)
+        every { isInDumbMode() } returns false
+        every { getService(DumbService::class.java) } returns dumbService
+    }
+    private val fileEditorManager = mock<FileEditorManager>().apply {
+        every { project } returns this@FileOpenedListenerTest.project
+        every { selectedEditor?.file } returns virtualFile
+    }
     private val fileEditorManagerEvent = mock<FileEditorManagerEvent>().apply {
         every { newFile } returns virtualFile
-        every { manager.project } returns project
+        every { manager } returns fileEditorManager
     }
 
     private val assistantViewService = mock<AssistantViewService>()
@@ -121,6 +135,45 @@ class FileOpenedListenerTest : KoinTest {
         target.selectionChanged(fileEditorManagerEvent)
 
         verify(exactly = 0) { assistantViewService.openFile(any()) }
+    }
+
+    @Test
+    fun `should not wait for dumb mode to finish if not in dumb mode`() {
+        target.selectionChanged(fileEditorManagerEvent)
+
+        verify(exactly = 0) { dumbService.runWhenSmart(any()) }
+    }
+
+    @Test
+    fun `should wait for dumb mode to finish before opening counterpart file`() {
+        every { project.isInDumbMode() } returns true
+
+        target.selectionChanged(fileEditorManagerEvent)
+
+        verify(exactly = 1) {
+            dumbService.runWhenSmart(any())
+            assistantViewService.openFile(assistantViewFile)
+        }
+    }
+
+    @Test
+    fun `should not try to open find or open counterpart file until dumb mode is finished`() {
+        every { project.isInDumbMode() } returns true
+        every { dumbService.runWhenSmart(any()) } answers { }
+
+        target.selectionChanged(fileEditorManagerEvent)
+
+        verify(exactly = 0) { assistantViewService.openFile(assistantViewFile) }
+    }
+
+    @Test
+    fun `should not try to open find or open counterpart file when dumb mode is finished if no editor is currently open`() {
+        every { project.isInDumbMode() } returns true
+        every { fileEditorManager.selectedEditor } returns null
+
+        target.selectionChanged(fileEditorManagerEvent)
+
+        verify(exactly = 0) { assistantViewService.openFile(assistantViewFile) }
     }
 
     private fun mockIntellijThreadControlFunctions() {
