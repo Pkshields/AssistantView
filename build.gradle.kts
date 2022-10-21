545dba6dff9f62bef2f1cd5e74b76f28ef6f3235
Paul Shields <pkshields@gmail.com>
2022-10-21 22:58:27 +0100

    Dependency version bump.

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,12 +19,12 @@ val detektConfigFile = files("detekt-config.yml")
 plugins {
     id("org.gradle.java-test-fixtures")
 
-    kotlin("jvm") version "1.6.21"
-    id("org.jetbrains.intellij") version "1.7.0"
+    kotlin("jvm") version "1.7.20"
+    id("org.jetbrains.intellij") version "1.9.0"
 
     id("net.saliman.properties") version "1.5.2"
-    id("io.gitlab.arturbosch.detekt") version "1.20.0"
-    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
+    id("io.gitlab.arturbosch.detekt") version "1.21.0"
+    id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
 }
 
 intellij {
@@ -79,21 +79,19 @@ allprojects {
     }
 
     dependencies {
-        implementation(kotlin("stdlib"))
-
         implementation("dev.paulshields:lok:1.0")
-        implementation("io.insert-koin:koin-core:3.2.0")
+        implementation("io.insert-koin:koin-core:3.2.2")
 
-        testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-        testImplementation("org.junit.jupiter:junit-jupiter-params:5.8.2")
+        testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+        testImplementation("org.junit.jupiter:junit-jupiter-params:5.9.0")
         testImplementation("com.willowtreeapps.assertk:assertk-jvm:0.25")
         testImplementation("org.awaitility:awaitility-kotlin:4.1.1")
 
         // Test fixtures dependencies marked as API to "leak" the dependencies into the test compile classpath
-        testFixturesApi("io.mockk:mockk:1.12.4")
-        testFixturesApi("io.insert-koin:koin-test:3.2.0")
-        testFixturesApi("io.insert-koin:koin-test-junit5:3.2.0")
+        testFixturesApi("io.mockk:mockk:1.13.2")
+        testFixturesApi("io.insert-koin:koin-test:3.2.2")
+        testFixturesApi("io.insert-koin:koin-test-junit5:3.2.2")
 
-        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
     }
 }
